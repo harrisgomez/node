2dff5ffdb59dc01943f1f2e22a9eb7fca36e2f1e
harrisgomez <node.harris@gmail.com>
2020-11-09 17:01:04 -0500

    Updated homepage.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the home page!');
+    res.send('Updated homepage');
 });
 
 const PORT = process.env.PORT || 3000;
